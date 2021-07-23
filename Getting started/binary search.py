205363a69d06a173516d412c8aeb6cc5aab51454
Dhruv Singh Jadon <75934644+Dhruvsinghjadon@users.noreply.github.com>
2021-07-23 10:00:21 +0530

    Create binary search.py

---
+++ Getting started/binary search.py
@@ -0,0 +1,29 @@
+import os
+import re
+import math
+import time
+
+def binary_search(array, searchingelement):
+	  last = array.__len__()
+	     mid = int(last/2)
+	         min = 0
+	 for i in range(int(math.log(last)/math.log(2)) + 1):
+		     if array[mid] == searchingelement:
+			       return str(mid) + " th index"
+		elif array[mid] < searchingelement:
+			min = mid
+			mid = int((last + mid) / 2)
+		      else:
+			         last = mid
+			             mid = int((mid + min) / 2)
+	return null
+		
+if __name__ == '__main__':
+	array = []
+	   for i in range(1000000):
+		       array.append(i)
+		
+	t0 = time.clock()
+	print binary_search(array, 345676)
+	t1 = time.clock()
+	print "binary_search: " + str(t1-t0)
